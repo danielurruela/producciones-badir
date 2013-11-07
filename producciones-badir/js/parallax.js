86052f8a80bbc6f4fb9b4a639b62eb0b949ca1f6
Daniel Urruela Salazar <daus888@hotmail.com>
2013-11-06 23:56:18 -0600

    TErimandas infografias malditas!

--- producciones-badir/js/parallax.js
+++ producciones-badir/js/parallax.js
@@ -4,10 +4,7 @@ $(document).ready(function(){
    enablePin:false,
    blocks:'.scrollblock'
    });
-scrollorama.animate('.main-statement',{property:'opacity', start:1,end:0});
 scrollorama.animate('#badir-pic',{duration:650, property:'bottom', start:-1000,end:-125});
 scrollorama.animate('#badir-conoceme',{duration:1000, property:'bottom', start:-400,end:-126});
 scrollorama.animate('#cubos',{duration:700, property:'bottom', start:-1000,end:300});
-
-
 });

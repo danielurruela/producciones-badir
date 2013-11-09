5e556480b3d1c97494bafa4f727da80751ea456d
Daniel Urruela Salazar <daus888@hotmail.com>
2013-11-08 23:22:30 -0600

    Terminada seccion de clientes, contacto y extras

--- producciones-badir/js/scripts.js
+++ producciones-badir/js/scripts.js
@@ -37,6 +37,44 @@ $(document).ready(function(){
                 window.location.hash = target;
             });
         });
+
+
+        $('a[href^="#clientes"]').on('click',function (e) {
+            e.preventDefault();
+
+            var target = this.hash,
+            $target = $(target);
+
+            $('html, body').stop().animate({
+                'scrollTop': $target.offset().top
+            }, 1500, 'swing', function () {
+                window.location.hash = target;
+            });
+        });
+        $('a[href^="#contacto"]').on('click',function (e) {
+            e.preventDefault();
+
+            var target = this.hash,
+            $target = $(target);
+
+            $('html, body').stop().animate({
+                'scrollTop': $target.offset().top
+            }, 1500, 'swing', function () {
+                window.location.hash = target;
+            });
+        });     
+        $('a[href^="#extras"]').on('click',function (e) {
+            e.preventDefault();
+
+            var target = this.hash,
+            $target = $(target);
+
+            $('html, body').stop().animate({
+                'scrollTop': $target.offset().top
+            }, 1500, 'swing', function () {
+                window.location.hash = target;
+            });
+        });                 
         $(".absolutepos").each(function() {
         $(this).parent().css("height",$(this).height());
         });           

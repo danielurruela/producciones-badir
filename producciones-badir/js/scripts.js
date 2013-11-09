6bbbdb753463a867ba539e563de098bf6d15a82a
Daniel Urruela Salazar <daus888@hotmail.com>
2013-11-09 12:43:07 -0600

    terminado efecto parallax en todo el sitio

--- producciones-badir/js/scripts.js
+++ producciones-badir/js/scripts.js
@@ -25,6 +25,19 @@ $(document).ready(function(){
             });
         }); 
 
+        $('a[href^="#hola"]').on('click',function (e) {
+            e.preventDefault();
+
+            var target = this.hash,
+            $target = $(target);
+
+            $('html, body').stop().animate({
+                'scrollTop': $target.offset().top
+            }, 1500, 'swing', function () {
+                window.location.hash = target;
+            });
+        }); 
+
         $('a[href^="#conoceme"]').on('click',function (e) {
             e.preventDefault();
 
